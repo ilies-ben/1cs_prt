eb6d7f43e613e7efcea78fde663e1c83f113e55d
ilies-ben <82667892+ilies-ben@users.noreply.github.com>
2023-05-03 22:18:45 +0100

    mise a jour avec test_api

--- erp/urls.py
+++ erp/urls.py
@@ -17,7 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from test_api.views import ProductList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('erp_app.urls'))
+    path('',include('erp_app.urls')),
+    path('api/products/', ProductList.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
+    
+    
 ]+ static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT)
